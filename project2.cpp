a495f99ecba1f4b11b25780f54eab8ed50abcccc
DaniBron <40351390+DaniBron@users.noreply.github.com>
2020-03-09 19:17:56 -0400

    Add files via upload

---
+++ project2.cpp
@@ -0,0 +1,56 @@
+#include <iostream> 
+#include <vector> 
+
+void getChar();
+void lex();
+
+#define INT_LIT 10
+#define INDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
+
+int main(int argc, char const *argv[])
+{
+    /* check if the input file is right */
+
+    /* else read the first char */
+    getChar();
+    /* do while loop */
+    do
+    {
+        lex();
+    } while (/* while not equal to EOF */);
+    
+    return 0;
+}
+
+int lex()
+{
+    
+}
+
+class TableEntry
+{
+private:
+    /* data */
+public:
+    project2(/* args */);
+    ~project2();
+};
+
+project2::project2(/* args */)
+{
+    int* location;
+    char* type;
+    auto* symbol;
+}
+
+project2::~project2()
+{
+}
+
